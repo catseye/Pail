befa78fa7d7ecf0fc55f5902eeb1c3dd26d11e9e
Chris Pressey <cpressey@catseye.tc>
2019-03-18 11:58:05 +0000

    Stylistic tweaks only.

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@
 #
 #    haste-cabal install parsec-3.1.1
 #
-# Later versions may not work.  For example, 3.1.13.0 fails to build for me at:
+# Later versions might not work.  For example, 3.1.13.0 fails to build for me at:
 # Preprocessing library generic-deriving-1.12.3...
 # src/Generics/Deriving/TH/Pre4_9.hs:177:20:
 #     parse error on input ‘->’
 #
 
-if command -v hastec > /dev/null 2>&1; then
-  (cd src && hastec HasteMain.hs -o ../demo/pail.js )
+if command -v hastec >/dev/null 2>&1; then
+  ( cd src && hastec HasteMain.hs -o ../demo/pail.js )
 else
   echo "hastec not found, not building pail.js"
 fi
