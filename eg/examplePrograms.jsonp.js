2c64aa902967c6639991243444fa11662d804f68
Chris Pressey <cpressey@catseye.tc>
2019-03-25 11:11:02 +0000

    Add another example program.

--- eg/examplePrograms.jsonp.js
+++ eg/examplePrograms.jsonp.js
@@ -8,15 +8,19 @@ examplePrograms = [
         "filename": "cons.pail"
     }, 
     {
+        "contents": "[#fst [a b]]\n", 
+        "filename": "hash-sugar.pail"
+    }, 
+    {
+        "contents": "**[*let [[a b] *a]]\n", 
+        "filename": "let-basic.pail"
+    }, 
+    {
         "contents": "**[*let [\n     [sndg *[**[*uneval snd] **[*uneval g]]]\n     **[*let [\n          [g [x y]]\n          ***sndg\n       ]]\n  ]]\n", 
         "filename": "let-evaled.pail"
     }, 
     {
         "contents": "**[*let [\n     [g moo]\n     **[*let [\n          [consnull *[#g null]]\n          ***consnull\n       ]]\n  ]]\n", 
         "filename": "let-unevaled.pail"
-    }, 
-    {
-        "contents": "**[*let [[a b] *a]]\n", 
-        "filename": "let.pail"
     }
 ];
